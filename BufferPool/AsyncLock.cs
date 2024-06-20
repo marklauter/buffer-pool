ced630ef19e2284c45b34be98040cdba50cbd1c4
Mark Lauter <mark@marklauter.com>
2024-06-19 20:44:58 -0400

    renamed latch to async lock

--- BufferPool/AsyncLock.cs
+++ BufferPool/AsyncLock.cs
@@ -1,14 +1,14 @@
 ﻿namespace BufferPool;
 
-internal sealed class Latch
+internal sealed class AsyncLock
     : IDisposable
 {
     private readonly SemaphoreSlim latch = new(1, 1);
     private bool disposed;
 
-    public async Task CriticalSectionAsync(Action action, CancellationToken cancellationToken)
+    public async Task EnterAsync(Action action, CancellationToken cancellationToken)
     {
-        await latch.WaitAsync(cancellationToken);
+        await ThrowIfDisposed().latch.WaitAsync(cancellationToken);
         try
         {
             action();
@@ -19,9 +19,9 @@ internal sealed class Latch
         }
     }
 
-    public async ValueTask CriticalSectionAsync(Func<CancellationToken, ValueTask> func, CancellationToken cancellationToken)
+    public async ValueTask EnterAsync(Func<CancellationToken, ValueTask> func, CancellationToken cancellationToken)
     {
-        await latch.WaitAsync(cancellationToken);
+        await ThrowIfDisposed().latch.WaitAsync(cancellationToken);
         try
         {
             await func(cancellationToken);
@@ -32,9 +32,9 @@ internal sealed class Latch
         }
     }
 
-    public async ValueTask<T> CriticalSectionAsync<T>(Func<CancellationToken, ValueTask<T>> func, CancellationToken cancellationToken)
+    public async ValueTask<T> EnterAsync<T>(Func<CancellationToken, ValueTask<T>> func, CancellationToken cancellationToken)
     {
-        await latch.WaitAsync(cancellationToken);
+        await ThrowIfDisposed().latch.WaitAsync(cancellationToken);
         try
         {
             return await func(cancellationToken);
@@ -56,4 +56,8 @@ internal sealed class Latch
 
         disposed = true;
     }
+
+    private AsyncLock ThrowIfDisposed() => disposed
+        ? throw new ObjectDisposedException(nameof(AsyncLock))
+        : this;
 }
