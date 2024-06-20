c9e4754519ea1d036af6671aa1a0d0a609cb0e2d
Mark Lauter <mark@marklauter.com>
2024-06-19 20:45:25 -0400

    added pin extensions

---
+++ BufferPool/PinExtensions.cs
@@ -0,0 +1,12 @@
+﻿using static BufferPool.PageManager;
+
+namespace BufferPool;
+
+internal static class PinExtensions
+{
+    public static Pin Bump(this Pin pin, IReplacementPolicy<int> evictionPolicy)
+    {
+        evictionPolicy.Bump(pin.PageId);
+        return pin;
+    }
+}
